9296f8e03d9e37c0ee4d3b9efbff1627988a4cd6
Elijah Lofgren <elijahlofgren@gmail.com>
2015-11-12 22:01:22 -0600

    Set browser policy to fix "Refused to load the script" errors
    Solution posted here: http://stackoverflow.com/a/33685726/908677

--- code/server/startup.js
+++ code/server/startup.js
@@ -1,1 +1,8 @@
-Meteor.startup( () => Modules.server.startup() );
+// old, no policy
+//Meteor.startup( () => Modules.server.startup() );
+
+// new with policy
+Meteor.startup( () => {
+  BrowserPolicy.content.allowOriginForAll("https://js.stripe.com/")
+  BrowserPolicy.content.allowOriginForAll("https://checkout.stripe.com/")
+})
